d9d02bc2d3979647c7a7e6ec1f9a229f25256cc4
moro <morolt@gmail.com>
2019-01-14 15:24:39 +0800

    adds uncle blocks

--- services/exporter.js
+++ services/exporter.js
@@ -1,6 +1,6 @@
 const web3 = require('../lib/web3');
 const config = new(require('../config.js'))();
-const {Block, Transaction,TokenConfig,Account} =  require("../services/orm");
+const {Block,Uncle, Transaction,TokenConfig,Account} =  require("../services/orm");
 const Sequelize = require("sequelize");
 const Op = Sequelize.Op;
 const baseToken = config.baseToken;
@@ -22,6 +22,7 @@ async function listenBlock(blockNumber){
             nonce : result.nonce,
             parentHash: result.parentHash,
             receiptsRoot: result.receiptsRoot,
+            uncleCount:0,
             sha3Uncles: result.sha3Uncles,
             size: result.size,
             stateRoot:result.stateRoot,
@@ -30,6 +31,42 @@ async function listenBlock(blockNumber){
             transactionsRoot:result.transactionsRoot
         };
 
+        await web3.eth.getBlockUncleCount(result.number,async (e,n)=>
+          {
+            blockValue.uncleCount = n;
+            if(n > 0){
+              console.log("uncleCount", result.number, n)
+              for(let i=0;i<n;i++){
+                await web3.eth.getUncle(result.number,i,async (e,b)=>{
+                  if (e == null){
+                    b.blockNumber = result.number;
+                    b.position = i;
+                    await Uncle.findOrCreate(
+                      {
+                          where:{
+                            blockNumber:result.number,
+                            position:i
+                          },
+                        defaults:b
+                        }
+                      )
+                      .spread(async(e,created)=>{
+                        if (!created){
+                          Uncle.update(b,{
+                            where:{
+                              blockNumber:result.number,
+                              position:i
+                            }});
+                        }
+                      })
+                  }
+                }).catch((e)=>{
+                  console.log("getUncle",e)
+                })
+              }
+            }
+          }
+        );
 
         await Block.findOrCreate({
             where:{
@@ -64,7 +101,9 @@ async function listenBlock(blockNumber){
                      }
                  }).spread(async (tx,c1) => {});
              })
-          });
+          }).catch((e)=>{
+          console.log("transactions",e)
+        });
 
            // await Transaction.findOrCreate({
            //    where:{
--- services/orm.js
+++ services/orm.js
@@ -1,230 +1,288 @@
 let db = require('../lib/db');
-let Sequelize = require("sequelize");
-const config = new(require('../config.js'))();
+let sequelize = require("sequelize");
+const config = new (require('../config.js'))();
 
-let Block = db.define('blocks',{
-    height : {
-        type : Sequelize.INTEGER,
-        primaryKey: true
-    },
-    difficulty : {
-        type : Sequelize.STRING
-    },
-    extraData :{
-        type : Sequelize.BLOB
-    },
-    gasLimit :{
-        type : Sequelize.INTEGER
-    },
-    gasUsed:{
-        type : Sequelize.INTEGER
-    },
-    hash : {
-        type : 'binary(66)' ,
-        primaryKey: true
-    },
-    logsBloom :{
-        type : Sequelize.BLOB
-    },
-    miner: {
-        type : 'binary(42)'
-    },
-    mixHash : {
-        type : 'binary(66)'
-    },
-    nonce : {
-        type : 'binary(18)'
-    },
-    parentHash : {
-        type : 'binary(66)'
-    },
-    receiptsRoot : {
-        type : 'binary(66)'
-    },
-    sha3Uncles : {
-        type : 'binary(66)'
-    },
-    size : {
-        type : Sequelize.INTEGER
-    },
-    stateRoot : {
-        type : 'binary(66)'
-    },
-    timestamp : {
-      type: Sequelize.INTEGER
-    },
-    totalDifficulty : {
-        type : Sequelize.STRING
-    },
-    transactionsRoot : {
-        type : 'binary(66)'
-    }
+const {
+  INTEGER,
+  STRING,
+  DATE,
+  NOW,
+  BLOB,
+  BOOLEAN,
+  BIGINT
+} = sequelize;
+
+let Block = db.define('blocks', {
+  height: {
+    type: INTEGER,
+    primaryKey: true
+  },
+  difficulty: {
+    type: STRING
+  },
+  extraData: {
+    type: BLOB
+  },
+  gasLimit: {
+    type: INTEGER
+  },
+  gasUsed: {
+    type: INTEGER
+  },
+  hash: {
+    type: 'binary(66)',
+    primaryKey: true
+  },
+  logsBloom: {
+    type: BLOB
+  },
+  miner: {
+    type: 'binary(42)'
+  },
+  mixHash: {
+    type: 'binary(66)'
+  },
+  nonce: {
+    type: 'binary(18)'
+  },
+  parentHash: {
+    type: 'binary(66)'
+  },
+  receiptsRoot: {
+    type: 'binary(66)'
+  },
+  uncleCount: {
+    type: INTEGER
+  },
+  sha3Uncles: {
+    type: 'binary(66)'
+  },
+  size: {
+    type: INTEGER
+  },
+  stateRoot: {
+    type: 'binary(66)'
+  },
+  timestamp: {
+    type: INTEGER
+  },
+  totalDifficulty: {
+    type: STRING
+  },
+  transactionsRoot: {
+    type: 'binary(66)'
+  }
 }, {
-    freezeTableName: true,
-    timestamps: false
-})
+  freezeTableName: true,
+  timestamps: false
+});
 
 let Transaction = db.define('transactions', {
-    blockHash : {
-        type: 'binary(66)'
-    },
-    blockNumber : {
-        type: Sequelize.INTEGER
-    },
-    hash : {
-        type: 'binary(66)',
-        primaryKey: true
-    },
-    from : {
-        type: 'binary(42)'
-    },
-    gas : {
-        type : Sequelize.INTEGER
-    },
-    gasUsed : {
-        type : Sequelize.INTEGER
-    },
-    gasPrice : {
-        type : Sequelize.BIGINT
-    },
-    input :{
-        type : Sequelize.BLOB
-    },
-    nonce : {
-        type : Sequelize.INTEGER
-    },
-    to : {
-        type: 'binary(42)'
-    },
-    transactionIndex : {
-        type : Sequelize.INTEGER
-    },
-    value : {
-      type: Sequelize.STRING
-    },
-    status : {
-      type: Sequelize.BOOLEAN
-    }
+  blockHash: {
+    type: 'binary(66)'
+  },
+  blockNumber: {
+    type: INTEGER
+  },
+  hash: {
+    type: 'binary(66)',
+    primaryKey: true
+  },
+  from: {
+    type: 'binary(42)'
+  },
+  gas: {
+    type: INTEGER
+  },
+  gasUsed: {
+    type: INTEGER
+  },
+  gasPrice: {
+    type: BIGINT
+  },
+  input: {
+    type: BLOB
+  },
+  nonce: {
+    type: INTEGER
+  },
+  to: {
+    type: 'binary(42)'
+  },
+  transactionIndex: {
+    type: INTEGER
+  },
+  value: {
+    type: STRING
+  },
+  status: {
+    type: BOOLEAN
+  }
 }, {
-    freezeTableName: true,
-    timestamps: false
+  freezeTableName: true,
+  timestamps: false
+});
+
+let Uncle = db.define('uncles', {
+  blockNumber: {type: INTEGER},
+  position: {type: INTEGER},
+
+  number: {type: INTEGER},
+  difficulty: {type: BIGINT},
+  extraData: {type: STRING(66)},
+  gasLimit: {type: INTEGER},
+  gasUsed: {type: INTEGER},
+  hash: {type: STRING(66), primaryKey: true},
+  logsBloom: {type: STRING(1024)},
+  miner: {type: STRING(42)},
+  mixHash: {type: STRING(66)},
+  nonce: {type: STRING},
+  parentHash: {type: STRING(66)},
+  receiptsRoot: {type: STRING(66)},
+  sha3Uncles: {type: STRING(66)},
+  size: {type: INTEGER},
+  stateRoot: {type: STRING(66)},
+  timestamp: {type: INTEGER},
+  totalDifficulty: {type: BIGINT},
+  transactionsRoot: {type: STRING(66)},
+
+}, {
+  freezeTableName: true,
+  timestamps: false,
+  indexes: [
+    {
+      name: 'blockNumber',
+      fields: ['blockNumber', 'position'],
+    },
+    {
+      name: 'uncleNumber',
+      fields: ['number'],
+    },
+    {
+      fields: ['miner'],
+    }
+  ]
+});
+
+let TokenConfig = db.define('token_configs', {
+  id: {
+    type: INTEGER,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  name: {
+    type: STRING(10)
+  },
+  address: {
+    type: STRING(42)
+  },
+  decimals: {
+    type: INTEGER
+  },
+  value: {
+    type: INTEGER
+  }
+}, {
+  freezeTableName: true,
+  timestamps: false
 });
 
 
-let TokenConfig = db.define('token_configs',{
-    id : {
-        type: Sequelize.INTEGER,
-        primaryKey: true,
-        autoIncrement: true
-    },
-    name : {
-        type: Sequelize.STRING(10)
-    },
-    address : {
-        type: Sequelize.STRING(42)
-    },
-    decimals : {
-        type: Sequelize.INTEGER
-    },
-    value :{
-        type: Sequelize.INTEGER
-    }
+let EventLog = db.define('events', {
+  address: {
+    type: 'binary(42)'
+  },
+  blockNumber: {
+    type: INTEGER
+  },
+  transactionHash: {
+    type: 'binary(66)',
+    primaryKey: true
+  },
+  transactionIndex: {
+    type: INTEGER
+  },
+  blockHash: {
+    type: 'binary(66)'
+  },
+  logIndex: {
+    type: INTEGER
+  },
+  removed: {
+    type: BOOLEAN
+  },
+  event: {
+    type: STRING(20)
+  },
+  id: {
+    type: STRING(20)
+  },
+  from: {
+    type: 'binary(42)'
+  },
+  to: {
+    type: 'binary(42)'
+  },
+  value: {
+    type: STRING
+  }
 }, {
-    freezeTableName: true,
-    timestamps: false
-})
-
-
-let EventLog = db.define('events',{
-    address : {
-        type: 'binary(42)'
-    },
-    blockNumber : {
-        type: Sequelize.INTEGER
-    },
-    transactionHash : {
-        type: 'binary(66)',
-        primaryKey: true
-    },
-    transactionIndex : {
-        type: Sequelize.INTEGER
-    },
-    blockHash : {
-        type: 'binary(66)'
-    },
-    logIndex : {
-        type: Sequelize.INTEGER
-    },
-    removed : {
-        type: Sequelize.BOOLEAN
-    },
-    event : {
-        type: Sequelize.STRING(20)
-    },
-    id : {
-        type: Sequelize.STRING(20)
-    },
-    from : {
-        type: 'binary(42)'
-    },
-    to : {
-        type: 'binary(42)'
-    },
-    value : {
-        type: Sequelize.STRING
-    }
-}, {
-    freezeTableName: true,
-    timestamps: false
-})
-
-
-let Account = db.define('accounts',{
-    id : {
-        type: Sequelize.INTEGER,
-        primaryKey: true,
-        autoIncrement: true
-    },
-    address : {
-        type: Sequelize.STRING(42),
-        unique : "t_balance_uk"
-    },
-    tokenAddress : {
-        type: Sequelize.STRING(42),
-        unique : "t_balance_uk"
-    },
-    value : {
-        type: Sequelize.STRING
-    }
-}, {
-    freezeTableName: true,
-    timestamps: false
+  freezeTableName: true,
+  timestamps: false
 });
 
-async function initTables(){
-    await Block.sync({ force: false});
-    await Transaction.sync({ force: false});
-    await TokenConfig.sync({ force: false});
-    await EventLog.sync({ force: false});
-    await Account.sync({ force: false});
-    await TokenConfig.findOrCreate({where: {name: config.baseToken}, defaults: {address: config.baseToken,decimals: 18, value: 0}})
-    initTableRelation()
+
+let Account = db.define('accounts', {
+  id: {
+    type: INTEGER,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  address: {
+    type: STRING(42),
+    unique: "t_balance_uk"
+  },
+  tokenAddress: {
+    type: STRING(42),
+    unique: "t_balance_uk"
+  },
+  value: {
+    type: STRING
+  }
+}, {
+  freezeTableName: true,
+  timestamps: false
+});
+
+async function initTables() {
+  await Block.sync({force: false});
+  await Transaction.sync({force: false});
+  await Uncle.sync({force: false});
+  await TokenConfig.sync({force: false});
+  await EventLog.sync({force: false});
+  await Account.sync({force: false});
+  await TokenConfig.findOrCreate({
+    where: {name: config.baseToken},
+    defaults: {address: config.baseToken, decimals: 18, value: 0}
+  })
+  initTableRelation()
 }
 
-function initTableRelation(){
-    Block.hasMany(Transaction, {foreignKey: 'blockHash', sourceKey: 'hash'});
-    Transaction.belongsTo(Block, {foreignKey: 'blockHash', targetKey: 'hash',as: 'block'});
-    Transaction.belongsTo(EventLog, {foreignKey: 'hash', targetKey: 'transactionHash',as: 'event'});
-    TokenConfig.belongsTo(EventLog, {foreignKey: 'address', targetKey: 'address',as: 'config'});
-    Account.belongsTo(TokenConfig, {foreignKey: 'tokenAddress', targetKey: 'address',as: 'config'});
+function initTableRelation() {
+  Block.hasMany(Transaction, {foreignKey: 'blockHash', sourceKey: 'hash'});
+  Transaction.belongsTo(Block, {foreignKey: 'blockHash', targetKey: 'hash', as: 'block'});
+  Transaction.belongsTo(EventLog, {foreignKey: 'hash', targetKey: 'transactionHash', as: 'event'});
+  TokenConfig.belongsTo(EventLog, {foreignKey: 'address', targetKey: 'address', as: 'config'});
+  Account.belongsTo(TokenConfig, {foreignKey: 'tokenAddress', targetKey: 'address', as: 'config'});
 }
 
 initTables();
 
 module.exports = {
-    TokenConfig,
-    Block,
-    Transaction,
-    EventLog,
-    Account
+  TokenConfig,
+  Block,
+  Uncle,
+  Transaction,
+  EventLog,
+  Account
 };
