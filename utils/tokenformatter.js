f966ea0a8d91bdb2fb0c0d6745b2d2c6c73cb1a6
waterlord <waterlord@vip.qq.com>
2019-07-12 14:19:44 +0800

    improve

--- utils/tokenformatter.js
+++ utils/tokenformatter.js
@@ -1,7 +1,13 @@
 var BigNumber = require('bignumber.js');
+var web3 = require('../lib/web3');
 
 function formatter() {
     this.format = function(value,decimal) {
+
+        if (decimal == 18){
+            return web3.utils.fromWei(value, "ether");
+        }
+
        return BigNumber(value).div(10 ** decimal).toString();
     }
 }
