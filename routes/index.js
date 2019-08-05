acf427bced559acfd2b8cbf5e77c393b04bf77db
waterlord <waterlord@vip.qq.com>
2019-08-05 17:13:03 +0800

    Revert "dev"
    This reverts commit 928f3987c239921c14b1567d2e2cc18cafc8d834.

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,25 @@ const db =  require("../lib/db");
 const Sequelize = require("sequelize");
 /* GET home page. */
 router.get('/', async function(req, res, next) {
+  // let blockParams = {
+  //   order : [
+  //     ['height','DESC']
+  //   ],
+	// 	offset:0,
+	// 	limit:15
+  // };
+  //
+  //
+  // let txParams = {
+  //   order : [
+  //     ['blockNumber','DESC']
+  //   ],
+	// 	offset:0,
+	// 	limit:15
+  // };
+  //
+  // var blocks = await Block.findAll(blockParams);
+  // var transactions = await Transaction.findAll(txParams);
 
     let blocks = await db.query(`
             select * from blocks order by number desc limit 15
